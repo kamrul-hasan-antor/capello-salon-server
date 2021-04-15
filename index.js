0d0b6642d341a397d302a5d0a842f8de0a44fcf6
kamrul-hasan-antor <kamrulhasan.antor95@gmail.com>
2021-04-16 02:20:31 +0600

    home done

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dcrxy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("hello world");
