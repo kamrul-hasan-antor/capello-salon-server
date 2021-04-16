da60ae65d775281f8102c84f2eb69ad82f4790c4
kamrul-hasan-antor <kamrulhasan.antor95@gmail.com>
2021-04-17 04:12:03 +0600

    services added

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+const port = process.env.PORT || 5000;
+
 require("dotenv").config();
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
-const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dcrxy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -14,13 +18,27 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  client.close();
+  const serviceCollection = client
+    .db(`${process.env.DB_NAME}`)
+    .collection(`${process.env.DB_COLLECTION}`);
+  // client.close();
+
+  app.get("/services", (req, res) => {
+    serviceCollection.find().toArray((err, items) => {
+      res.send(items);
+    });
+  });
+
+  app.post("/addServices", (req, res) => {
+    const newService = req.body;
+    serviceCollection.insertOne(newService).then((result) => {
+      res.send(result.insertedCount > 0);
+      res.redirect("/");
+    });
+  });
 });
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-app.listen(port, () => {
-  console.log(`listening ar http://localhost:${port}`);
-});
+app.listen(port);
